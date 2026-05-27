d2523e44ca808984817353fab0ad2126a8d80abf
Vipericc <mocuishle0529@gmail.com>
2026-05-27 23:28:03 +0800

    流水灯控制代码

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <Arduino.h>
 
-int count = 0;
-
 void setup() 
 {
     Serial.begin(115200);  
     pinMode(2, OUTPUT);
+    pinMode(4, OUTPUT);
     pinMode(16, OUTPUT);
+    pinMode(17, OUTPUT);
     pinMode(5, OUTPUT);
-    pinMode(19, OUTPUT);
-
+    pinMode(18, OUTPUT);
 }
 
 void loop() 
@@ -17,13 +16,19 @@ void loop()
     digitalWrite(2, HIGH);
     delay(500);
     digitalWrite(2, LOW);
+    digitalWrite(4, HIGH);
+    delay(500);
+    digitalWrite(4, LOW);
     digitalWrite(16, HIGH);
     delay(500);
     digitalWrite(16, LOW);
+    digitalWrite(17, HIGH);
+    delay(500);
+    digitalWrite(17, LOW);
     digitalWrite(5, HIGH);
     delay(500);
     digitalWrite(5, LOW);
-    digitalWrite(19, HIGH);
+    digitalWrite(18, HIGH);
     delay(500);
-    digitalWrite(19, LOW);
+    digitalWrite(18, LOW);
 }
